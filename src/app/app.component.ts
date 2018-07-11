1383bf8fdb4514fff697b7831c95dc57cc27d1c6
busby-thinkpad <michaelb@clearlyinnovative.com>
2018-07-11 14:21:55 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loaded = false;
+
+  constructor() {
+    this.loaded = true;
+  }
 }
